297508166841a7468e717ad4bd2a64d3875d81fc
juliz-lab <julkucin@gmail.com>
2025-11-05 11:43:00 +0200

    Update mylib.h
    Sukurta spausdinimo funkcija.

--- mylib.h
+++ mylib.h
@@ -9,6 +9,7 @@
 #include <map>
 #include <list>
 #include <chrono>
+#include <iterator>
 
 using std::cin;
 using std::cout;
@@ -17,24 +18,23 @@ using std::fixed;
 using std::ifstream;
 using std::istringstream; // stringe esančių žodžių sk skaičiavimui
 using std::left;
+using std::list;
+using std::map;
+using std::move;
 using std::mt19937;
+using std::ofstream;
+using std::partition;
 using std::random_device;
 using std::right;
 using std::setprecision;
 using std::setw;
 using std::sort;
 using std::string;
+using std::stringstream;
+using std::to_string;
 using std::uniform_int_distribution;
 using std::vector;
 using std::ws;
-using std::ofstream;
-using std::stringstream;
-using std::to_string;
-using std::map;
-using std::list;
-using std::sort;
-using std::partition;
-using std::move;
 
 struct Studentas
 {
@@ -53,3 +53,20 @@ void pasiskirstymas_vector_2(vector<Studentas> Grupe, const int &irasu_sk);
 void pasiskirstymas_list_2(list<Studentas> Grupe, const int &irasu_sk);
 void pasiskirstymas_vector_3(vector<Studentas> &Grupe, const int &irasu_sk);
 void pasiskirstymas_list_3(list<Studentas> &Grupe, const int &irasu_sk);
+vector<Studentas> Failo_nuskaitymas(const string &failas);
+
+template <typename Container>
+void Spausdink(const Container &moksliukai, const Container &nemoksos, const string &strategija, const string &konteineris)
+{
+    string sablonas = "rezultatai_" + strategija + "_" + konteineris;
+    ofstream frM(sablonas + "_moksliukai.txt");
+    ofstream frN(sablonas + "_nemoksos.txt");
+    for (auto it = std::begin(moksliukai); it != std::end(moksliukai); ++it)
+    {
+        frM << it->var << ' ' << it->pav << ' ' << it->gal << endl;
+    }
+    for (auto it = std::begin(nemoksos); it != std::end(nemoksos); ++it)
+    {
+        frN << it->var << ' ' << it->pav << ' ' << it->gal << endl;
+    }
+}
